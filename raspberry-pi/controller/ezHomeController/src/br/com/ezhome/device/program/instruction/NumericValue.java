0aa65959b55309ca7f96efdfc8b653a09dcfac2d
Cristofer Zdepski <cristoferz@gmail.com>
2016-04-04 13:31:23 -0300

    Adicionada a programação de valores numericos (a concluir)

---
+++ raspberry-pi/controller/ezHomeController/src/br/com/ezhome/device/program/instruction/NumericValue.java
@@ -0,0 +1,11 @@
+package br.com.ezhome.device.program.instruction;
+
+/**
+ *
+ * @author cristofer
+ */
+public class NumericValue {
+   
+   private boolean constant, typeFloat;
+   private int length;
+}
